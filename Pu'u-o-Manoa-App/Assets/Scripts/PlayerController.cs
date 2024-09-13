e9ad1fb79e0281706602b262ae4bc67043f2bed7
IsaacVerbrugge25 <personontheinternet1234@gmail.com>
2024-09-13 09:50:56 -1000

    tree and player control

--- Pu'u-o-Manoa-App/Assets/Scripts/PlayerController.cs
+++ Pu'u-o-Manoa-App/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,17 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    Rigidbody rb;
+    public float jumpForce;
+    bool canJump;
+
+    // Called before start
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +24,30 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+
+        if (Input.GetKeyDown(KeyCode.Space) && canJump)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+
+
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            canJump = true;
+        }
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            canJump = false;
+        }
+    }
+
 }
