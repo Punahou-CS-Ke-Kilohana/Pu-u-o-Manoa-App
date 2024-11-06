cf48c2d6adc7b8b1484f94c2e9a5c6feadfea2da
chostmadsen25 <143050709+githubCHM@users.noreply.github.com>
2024-11-06 09:58:24 -1000

    Update model_v2.py
    added some visualization (nothing is training)

--- python_backend/model_fixing/model_v2.py
+++ python_backend/model_fixing/model_v2.py
@@ -1,15 +1,19 @@
 import sys
 import os
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 
 import torch
-import torchvision
 import torchvision.transforms as transforms
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
 
-from python_backend.core.dataloader import train_dataloader
+from tqdm import tqdm
+import matplotlib.pyplot as plt
+
+from python_backend.core.dataloader import train_dataloader, \
+    test_dataloader  # Assume you have a separate test_dataloader
 
 
 class Net(nn.Module):
@@ -18,28 +22,27 @@ class Net(nn.Module):
         # First conv block: 256 -> 252 -> 126
         self.conv1 = nn.Conv2d(3, 16, 5)
         self.pool = nn.MaxPool2d(2, 2)
-        
+
         # Second conv block: 126 -> 122 -> 61
         self.conv2 = nn.Conv2d(16, 32, 5)
-        
+
         # Third conv block: 61 -> 57 -> 28
         self.conv3 = nn.Conv2d(32, 64, 5)
-        
+
         # Fourth conv block: 28 -> 24 -> 12
         self.conv4 = nn.Conv2d(64, 64, 5)
-        
+
         # Final feature map will be 64 x 12 x 12 = 9216
         self.fc1 = nn.Linear(64 * 12 * 12, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, 15)
 
     def forward(self, x):
-        # Apply conv blocks with ReLU and pooling
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         x = self.pool(F.relu(self.conv3(x)))
         x = self.pool(F.relu(self.conv4(x)))
-        
+
         x = torch.flatten(x, 1)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
@@ -52,14 +55,38 @@ batch_size = 4
 trainset = train_dataloader.dataset
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True, num_workers=0)
 
+testset = test_dataloader.dataset
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=False, num_workers=0)
+
 classes = [str(i) for i in list(range(15))]
+iter_nums = []
+logged_losses = []
+
+
+def test_accuracy(net, testloader):
+    # this is fried
+    correct = 0
+    total = 0
+    net.eval()  # Set model to evaluation mode
+    with torch.no_grad():  # Disable gradient calculation for testing
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)  # Shape should be (batch_size, 15)
+            _, predicted = torch.max(outputs, 1)  # Get the predicted class indices
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()  # Compare predictions with actual labels
+    net.train()  # Set model back to training mode
+    print(f'Accuracy of the network on the test images: {100 * correct / total:.2f}%')
+
+
+
 
 def main():
     net = Net()
     criterion = nn.CrossEntropyLoss()
-    optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+    optimizer = optim.Adam(net.parameters(), lr=0.005)
 
-    for epoch in range(2):
+    for epoch in range(5):
         running_loss = 0.0
         for i, data in enumerate(trainloader, 0):
             inputs, labels = data
@@ -70,14 +97,23 @@ def main():
             optimizer.step()
 
             running_loss += loss.item()
-            if i % 2000 == 1999:
-                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss / 2000:.3f}')
+            if i % 1 == 0:
+                iter_nums.append(i + epoch * len(trainloader))
+                logged_losses.append(running_loss)
+                print(f'[{epoch + 1}, {i + 1:5d}] loss: {running_loss:.3f}')
                 running_loss = 0.0
 
     print('Finished Training')
+    # test_accuracy(net, testloader)  # Evaluate test accuracy after training
+
 
 if __name__ == "__main__":
     import torch.multiprocessing as mp
+
     mp.freeze_support()
     mp.set_start_method('spawn', force=True)
     main()
+    plt.plot(iter_nums, logged_losses)
+    plt.xlabel('Iterations')
+    plt.ylabel('Loss')
+    plt.show()
