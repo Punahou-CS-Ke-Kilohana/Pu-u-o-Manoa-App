5cf98d201ebba2fad4c7959a54333d7a2bd9f565
Derek Yee <derek.s.yee@gmail.com>
2024-12-06 10:00:34 -1000

    more species displant
    TODO add bio for plants

---
+++ Pu'u-o-Manoa-App/Assets/Scripts/MoreSpeciesDisplay.cs
@@ -0,0 +1,41 @@
+// uses functions from the plant load to put data onto screen
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MoreSpeciesDisplay : MonoBehaviour
+{
+    // Assuming PlantLoader loads and manages PlantData
+    private PlantLoader plantLoader = new PlantLoader();
+    public TMP_Text speciesNameText; // TextMeshPro element for displaying species name
+    public TMP_Text hawaiianNameText; // TextMeshPro element for displaying Hawaiian name
+    public TMP_Text speciesStatusText; // TextMeshPro element for displaying Species Statu name
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (PlayerPrefs.HasKey("PlantName"))
+        {
+            string plantName = PlayerPrefs.GetString("PlantName");
+            speciesNameText.text = plantName; // Update the text in the UI
+        }
+        else
+        {
+            speciesNameText.text = "Unknown Plant"; // Fallback text
+        }
+
+        // Ensure speciesName.text has a valid value
+        if (!string.IsNullOrEmpty(speciesNameText.text))
+        {
+            // Get Hawaiian names for the species name
+            List<string> hawaiianNames = plantLoader.GetHawaiianNames(speciesNameText.text);
+
+            string speciesStatus = plantLoader.GetConservationStatus(speciesNameText.text);
+
+            hawaiianNameText.text = string.Join(", ", hawaiianNames);
+            speciesStatusText.text = speciesStatus;
+        }
+    }
+}
