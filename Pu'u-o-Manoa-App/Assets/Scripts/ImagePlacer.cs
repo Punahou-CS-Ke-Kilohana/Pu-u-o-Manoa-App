b799605f14f22002e49b5a15c4674e62cd087404
IsaacVerbrugge25 <personontheinternet1234@gmail.com>
2025-01-23 10:43:45 -1000

    start on imageplacer

---
+++ Pu'u-o-Manoa-App/Assets/Scripts/ImagePlacer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float startLat = 3.5f;
+    public float startLon = 0.1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Place(float Latitude, float Longitude, string image)
+    {
+
+    }
+
+}
