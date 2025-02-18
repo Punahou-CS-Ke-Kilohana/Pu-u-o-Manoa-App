f1f9a1b027ad0842ed5198e72c1c4ce51ba8771f
chostmadsen25 <143050709+githubCHM@users.noreply.github.com>
2025-02-18 09:20:47 -1000

    Update training_config.py
    fixed typo that causes older versions of python to break

--- ml_backend/application/configs/training_config.py
+++ ml_backend/application/configs/training_config.py
@@ -25,8 +25,8 @@ training_config.save_params = {
     'save_root': os.path.join(os.path.dirname(os.path.dirname(os.path.dirname(__file__))), 'models'),
     'save_name': f"model_"
                  f"{datetime.datetime.now().year}_"
-                 f"{str(datetime.datetime.now().month).zfill(2)}_{
-                 str(datetime.datetime.now().day).zfill(2)}"
+                 f"{str(datetime.datetime.now().month).zfill(2)}_"
+                 f"{str(datetime.datetime.now().day).zfill(2)}"
 }
 training_config.save_gap = 25
 
